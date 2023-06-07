7c6ac64b18f92d6e9c44f735af82e505099f7ea1
Vincent Rabatel <vincent.rabatel@gmail.com>
2023-06-07 13:30:16 +0200

    Add some HTML and CSS for edit mode

--- assets/edit.js
+++ assets/edit.js
@@ -1,6 +1,7 @@
-// Get HTML elements
+// Get edit mode HTML elements and create event listeners
 const editButtonElement = document.querySelector(".edit-button"); console.log(editButtonElement);
 const publishButtonElement = document.querySelector(".publish-button"); console.log(publishButtonElement);
+
 
 editButtonElement.addEventListener('click', function(){
     console.log("Edit button clicked !");
@@ -8,4 +9,16 @@ editButtonElement.addEventListener('click', function(){
 
 publishButtonElement.addEventListener('click', function(){
     console.log("Publish button clicked !");
+}, false);
+
+// Get modal HTML elements and create event listeners
+const closeButtonElement = document.querySelector(".close-button"); console.log(closeButtonElement);
+const backButtonElement = document.querySelector(".back-button"); console.log(backButtonElement);
+
+closeButtonElement.addEventListener('click', function(){
+    console.log("Close button clicked !");
+}, false);
+
+backButtonElement.addEventListener('click', function(){
+    console.log("Back button clicked !");
 }, false);
