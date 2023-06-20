3cef75ab85cfd2451c10a0ee63b1c26e8facd721
Vincent Rabatel <vincent.rabatel@gmail.com>
2023-06-20 14:57:40 +0200

    Clean last api function to post and delete projects from database

--- src/hooks/api.js
+++ src/hooks/api.js
@@ -2,64 +2,74 @@
 // API get and post functions //
 // -------------------------- //
 
-export async function getProject(projectId){
-	// TODO ?
-}
+import * as storage from '../hooks/storage.js';
 
 // Fetch all projects from API
 export async function getProjects(){
-	//console.log("Fetching projects form the API...");
+	console.log("API : Fetching projects form the API...");
 	const projectsResponse = await fetch("http://localhost:5678/api/works");
-	const projects = await projectsResponse.json(); //console.log(projects);
+	const projects = await projectsResponse.json();
 	
 	return projects;
 }
 
+// Add a project to the database
 export async function postProject(project){
-	console.log("Posting the new project number " + project.id + " to the API");
-	
+	console.log("API : Posting the new project number " + project.id + " to the API");
+
+	// Create a blob storing the project's image 
+	const blob = await fetch(project.imageUrl).then(r => r.blob());
+
+	// Create a form date to be send as the body of the fetch()
+	const projectData  = new FormData();
+
+	projectData.append("image", blob);
+	projectData.append("title", project.title);
+	projectData.append("category", project.categoryId);
+
+	// Get log in info to be able to use the auth token in the fetch()
+	const logInInfo = storage.getLogInInfos();
+
 	const postProjectResponse = await fetch("http://localhost:5678/api/works", {
 		method: 'POST',
 		headers: {
-			'Content-Type': 'multipart/form-data',
-			'Authorization': ''
+			'Authorization': 'Bearer ' + logInInfo.token
 		},
-		body: JSON.stringify(project)
+		body: projectData
 	});
 
 	console.log(postProjectResponse);
 }
 
+// Delete a project from the database
 export async function deleteProject(projectId){
-	console.log("Delete the project number " + projectId + " from the API");
+	console.log("API : Delete the project number " + projectId + " from the API");
 
-	const projectIdAsInt = parseInt(projectId); console.log(projectIdAsInt)
-	const deleteProjectResponse = await fetch("http://localhost:5678/api/works/" + projectIdAsInt, {
+	// Get log in info to be able to use the auth token in the fetch()
+	const logInInfo = storage.getLogInInfos();
+
+	const deleteProjectResponse = await fetch("http://localhost:5678/api/works/" + projectId, {
 		method: 'DELETE',
 		headers: {
-			'Authorization': ''
+			'Authorization': 'Bearer ' + logInInfo.token
 		}
 	});
 
 	console.log(deleteProjectResponse);
 }
 
-export async function getCategory(categoryId){
-	// TODO ?
-}
-
-// Fetch all categories from API
+// Fetch all categories from the database
 export async function getCategories(){
-	//console.log("Fetching categories form the API...");
+	console.log("API : Fetching categories form the API...");
 	const categoriesResponse = await fetch("http://localhost:5678/api/categories");
 	const categories = await categoriesResponse.json(); //console.log(categories);
 
 	return categories;
 }
 
-// Fetch log in response from API depending of log in info
+// Fetch log in response from API depending of log in form
 export async function postLogInInfo(logInInfo){
-	//console.log("Trying to log in...");
+	console.log("API : Trying to log in...");
 	const logInResponse = await fetch("http://localhost:5678/api/users/login", {
 		method: 'POST',
 		headers: {
@@ -69,4 +79,14 @@ export async function postLogInInfo(logInInfo){
 	});
 
 	return logInResponse;
+}
+
+
+// Backlog 
+export async function getProject(projectId){
+	// TODO ?
+}
+
+export async function getCategory(categoryId){
+	// TODO ?
 }
